30294a342a81bd01b5ad0e8efb904b11a01e8241
Florin Dirica <florin.dirica@mountsoftware.ro>
2024-02-06 18:00:14 +0200

    Set correct typings on provider api interface

--- src/Service/ProviderApiInterface.php
+++ src/Service/ProviderApiInterface.php
@@ -25,9 +25,10 @@ interface ProviderApiInterface
     /**
      * Saves the order data into the provider system and returns the external order ID, if it is applicable
      *
-     * @var ProviderOrderInterface $order
+     * @param ProviderOrderInterface $order
+     * @param ProviderCredentialsInterface $credentials
      */
-    public function sendOrderToProvider($order, ProviderCredentialsInterface $credentials): ?BaseProviderOrderResponse;
+    public function sendOrderToProvider($order, $credentials): ?BaseProviderOrderResponse;
 
     /**
      * Handles the mapping between ShoppingCart and ProviderOrder models
@@ -37,24 +38,19 @@ interface ProviderApiInterface
     public function mapShoppingCartToProviderOrder(Tenant $tenant, ShoppingCart $cart);
 
     /**
-     * @param Tenant $tenant
-     * @param ProviderOrderInterface $order
-     * @return ShoppingCart
      * Handles the mapping between ProviderOrder and ShoppingCart models
+     *
+     * @param ProviderOrderInterface $order
      */
     public function mapProviderOrderToShoppingCart(Tenant $tenant, $order): ShoppingCart;
 
     /**
-     * @param string $horecaExternalServiceId
      * @param ProviderCredentialsInterface $credentials
-     * @return bool
      */
     public function initializeShop(string $horecaExternalServiceId, $credentials): bool;
 
     /**
-     * @param HorecaRequestDeliveryBody $body
      * @param ProviderCredentialsInterface $credentials
-     * @return bool
      */
     public function requestDelivery(HorecaRequestDeliveryBody $body, $credentials): bool;
 
