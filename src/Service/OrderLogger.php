b050756fc65b4203b9521b3de81d720ffcb4406f
Florin Dirica <florin.dirica@mountsoftware.ro>
2024-02-07 20:51:09 +0200

    Fix a typo

--- src/Service/OrderLogger.php
+++ src/Service/OrderLogger.php
@@ -56,7 +56,7 @@ class OrderLogger
             return false;
         }
 
-        $sql = "INSERT INTO order_log (action, order_id, micro_time, level, log, created_at) VALUES (:action, :order_id, :micro_time, :level, :log, :created_at)";
+        $sql = "INSERT INTO hmc_order_logs (action, order_id, micro_time, level, log, created_at) VALUES (:action, :order_id, :micro_time, :level, :log, :created_at)";
         $params = [
             'action'     => $action,
             'order_id'   => $order->getId(),
