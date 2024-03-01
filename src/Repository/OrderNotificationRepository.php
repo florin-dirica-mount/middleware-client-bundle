10d0626e923dd3c29ed49d11187ea1e54380477a
Florin Dirica <florin.dirica@mountsoftware.ro>
2024-03-01 12:57:26 +0200

    Deprecate a method

--- src/Repository/OrderNotificationRepository.php
+++ src/Repository/OrderNotificationRepository.php
@@ -5,6 +5,7 @@ namespace Horeca\MiddlewareClientBundle\Repository;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Horeca\MiddlewareClientBundle\Entity\OrderNotification;
+use Horeca\MiddlewareClientBundle\Entity\Tenant;
 use Horeca\MiddlewareClientBundle\Enum\OrderNotificationStatus;
 
 /**
@@ -21,9 +22,17 @@ class OrderNotificationRepository extends ServiceEntityRepository
         parent::__construct($registry, OrderNotification::class);
     }
 
+    /**
+     * @deprecated use self::findOneByTenantOrderId
+     */
     public function findOneByHorecaOrderId(string $id): ?OrderNotification
     {
         return $this->findOneBy(['horecaOrderId' => $id]);
+    }
+
+    public function findOneByTenantOrderId(Tenant $tenant, string $id): ?OrderNotification
+    {
+        return $this->findOneBy(['tenant' => $tenant, 'tenantObjectId' => $id]);
     }
 
     public function save(OrderNotification $notification): void
