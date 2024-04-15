e18c80a739c8430db331f3e71eb660c0300b4dd6
gabriel cristea <gcristea07@gmail.com>
2024-04-15 20:02:43 +0300

    added serialisation groups

--- src/Enum/SerializationGroups.php
+++ src/Enum/SerializationGroups.php
@@ -6,4 +6,5 @@ final class SerializationGroups
 {
     public const Default = 'default';
     public const TenantOrderNotificationView = 'tenant:order_notification:view';
+    public const ProviderOrderNotificationView = 'provider:order_notification:view';
 }
