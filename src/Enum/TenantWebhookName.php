3d183359913a9ba288a7b23bade1f23d935c7929
gabriel cristea <gcristea07@gmail.com>
2024-05-14 19:35:34 +0300

    added new WEBHOOK name

--- src/Enum/TenantWebhookName.php
+++ src/Enum/TenantWebhookName.php
@@ -6,6 +6,8 @@ class TenantWebhookName
 {
     const WEBHOOK_SHOPPING_CART_SEND = 'tenant.webhook.shopping_cart.send';
     const WEBHOOK_ORDER_NOTIFICATION_EVENT = 'tenant.webhook.order_notification_event';
+    const WEBHOOK_GET_PRODUCTS = 'tenant.webhook.get_products';
+
 
     protected function __construct() { }
 }
