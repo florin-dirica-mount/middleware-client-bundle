a52445ce8149de19c7b37d50f53d90a826926d69
gabriel cristea <gcristea07@gmail.com>
2024-10-08 13:04:18 +0300

    updated OrderNotificationEventDto

--- src/VO/Api/HorecaDataUpdateEventDto.php
+++ src/VO/Api/HorecaDataUpdateEventDto.php
@@ -5,6 +5,7 @@ namespace Horeca\MiddlewareClientBundle\VO\Api;
 abstract class HorecaDataUpdateEventDto
 {
     public string $event;
+    public \DateTime $time;
 
     abstract function getData();
 
