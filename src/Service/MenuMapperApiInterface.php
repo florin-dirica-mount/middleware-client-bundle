c18409d6d77559b7f16a83dbc5e5f50a7a60aa69
gabriel cristea <gcristea07@gmail.com>
2024-10-29 12:39:03 +0200

    updated interfaces

--- src/Service/MenuMapperApiInterface.php
+++ src/Service/MenuMapperApiInterface.php
@@ -2,12 +2,13 @@
 
 namespace Horeca\MiddlewareClientBundle\Service;
 
+use Horeca\MiddlewareClientBundle\Entity\MappingNotification;
 use Horeca\MiddlewareClientBundle\Entity\MenuNotification;
 
 interface MenuMapperApiInterface
 {
-    public function mapTenantMenuToProvider(MenuNotification $notification): bool;
-    public function sendTenantMenuToProvider(MenuNotification $notification): bool;
+    public function mapTenantMenuToProvider(MenuNotification $notification): MappingNotification;
+    public function sendTenantMenuToProvider(MenuNotification $notification): MappingNotification;
 
 
 }
