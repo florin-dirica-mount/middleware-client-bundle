0e96742ead022a26ce8710b108e5d7cf3a9cd29d
gabriel cristea <gcristea07@gmail.com>
2025-01-13 11:54:00 +0200

    protocol update

--- src/Controller/HorecaApiController.php
+++ src/Controller/HorecaApiController.php
@@ -213,6 +213,9 @@ class HorecaApiController extends AbstractController
 
             $order->setTenant($tenant);
             $order->setTenantObjectId($body->cart->getId());
+            if($body->cart->getExternalProviderId()){
+                $order->setProviderObjectId($body->cart->getExternalProviderId());
+            }
             $order->setTenantPayloadString($this->serializer->serialize($body->cart, 'json'));
             $order->setTenantShopId($body->cart->getRestaurant()->getId());
 
