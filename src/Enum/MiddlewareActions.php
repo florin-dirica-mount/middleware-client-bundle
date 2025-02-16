99f7fcc54b033d1f4940d45cb6a7d9ec42ef9b4a
gabriel cristea <gcristea07@gmail.com>
2025-02-16 21:00:16 +0200

    Add new middleware actions for provider product operations

--- src/Enum/MiddlewareActions.php
+++ src/Enum/MiddlewareActions.php
@@ -7,5 +7,9 @@ class MiddlewareActions
     public const SyncTenantProducts = 'middleware.sync.tenant.products';
     public const SyncProviderProducts = 'middleware.sync.provider.products';
 
+    public const ExportProviderProducts = 'middleware.export.provider.products';
+    public const UpdateProviderProducts = 'middleware.update.provider.products';
+    public const SyncAndExportProviderProducts = 'middleware.sync.export.provider.products';
+
     protected function __construct() { }
 }
