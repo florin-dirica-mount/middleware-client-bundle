0cf9a67218123857977554f98480f8b1dedafc80
gabriel cristea <gcristea07@gmail.com>
2025-05-01 08:33:02 +0300

    Add WEBHOOK_EVENTS constant to TenantWebhookName enum

--- src/Enum/TenantWebhookName.php
+++ src/Enum/TenantWebhookName.php
@@ -8,6 +8,7 @@ class TenantWebhookName
     const WEBHOOK_SHOPPING_CART_UPDATE_SEND = 'tenant.webhook.shopping_cart_update.send';
     const WEBHOOK_ORDER_NOTIFICATION_EVENT = 'tenant.webhook.order_notification_event';
     const WEBHOOK_GET_PRODUCTS = 'tenant.webhook.get_products';
+    const WEBHOOK_EVENTS = 'tenant.webhook.events';
 
 
     protected function __construct() { }
