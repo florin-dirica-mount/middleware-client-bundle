d6adaa074eae07b82711dcf92fba08ea8577bf3b
gabriel cristea <gcristea07@gmail.com>
2025-09-08 17:57:43 +0300

    updated

--- src/Enum/TenantWebhookName.php
+++ src/Enum/TenantWebhookName.php
@@ -11,8 +11,10 @@ class TenantWebhookName
     const WEBHOOK_EVENTS = 'tenant.webhook.events';
 
     const WEBHOOK_MENU_CATEGORY_SEND = 'tenant.webhook.menu.category.send';
-    const WEBHOOK_MENU_DEPLOY = 'tenant.webhook.menu.deploy';
-    const WEBHOOK_MENU_REMOVE_LAST_UPLOADED = 'tenant.webhook.menu.remove_last_uploaded';
+    const WEBHOOK_MENU_SWITCH_WITH_TMP = 'tenant.webhook.menu.switch_with_tmp';
+    const WEBHOOK_MENU_REMOVE_TMP = 'tenant.webhook.menu.remove_tmp';
 
-    protected function __construct() { }
+    protected function __construct()
+    {
+    }
 }
