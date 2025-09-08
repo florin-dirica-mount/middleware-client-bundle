e896f9449a9bfa3483ae8275ba6da56e221ff56f
gabriel cristea <gcristea07@gmail.com>
2025-09-08 14:56:55 +0300

    updated

--- src/Service/TenantClient.php
+++ src/Service/TenantClient.php
@@ -9,7 +9,7 @@ use Horeca\MiddlewareClientBundle\Entity\TenantWebhook;
 use Horeca\MiddlewareCommonLib\Exception\HorecaException;
 use Psr\Http\Message\ResponseInterface;
 
-final class TenantClient implements TenantClientInterface
+class TenantClient implements TenantClientInterface
 {
     private Client $client;
 
